541523ac0c2f4ec1f89cf82a5854469623094bfd
Andrey Rumakin <rumakin.andrey@yandex.ru>
2022-11-24 10:51:00 +0300

    feat: added dragAndDrop to columns

--- src/pages/Board/Column/Column.tsx
+++ src/pages/Board/Column/Column.tsx
@@ -1,3 +1,4 @@
+import { Draggable } from 'react-beautiful-dnd';
 import { useTranslation } from 'react-i18next';
 
 import { Task } from '../Task/Task';
@@ -11,9 +12,9 @@ interface ColumnProps {
   column: IColumn;
   boardId: string;
   onDelete: (col: IColumn) => void;
-
+  index: number;
 }
-export const Column = ({ boardId, column, onDelete }: ColumnProps): JSX.Element => {
+export const Column = ({ boardId, column, onDelete, index }: ColumnProps): JSX.Element => {
 
   const { data: tasks, isLoading } =
     tasksApi.useGetTasksQuery({ boardId, colId: column.id });
@@ -50,51 +51,58 @@ export const Column = ({ boardId, column, onDelete }: ColumnProps): JSX.Element 
   };
 
   return (
-    <section className="column-wrapper">
-      {((isLoading || crLoading || delLoading) && <Loader />)}
-
-      <div className="column">
-        <h1 className="column-title">{column.title} (order: {column.order})</h1>
-
-        <button
-          type = 'button'
-          className="column-del"
-          onClick={()=>onDelete(column)}
+    <Draggable draggableId={column.id} index={index}>
+      {provided => (
+        <section className="column-wrapper"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="board-del-icon">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-          </svg>
-        </button>
+          {((isLoading || crLoading || delLoading) && <Loader />)}
 
-        <div className="column-body" >
-          {tasks && tasks.map(task =>
-            <Task
-              task={task}
-              //   boardId = {boardId}
-              //   colId = {column.id}
-              onDelete={handleDeleteTask}
-              key={task.id}
-            />,
-          )}
-        </div>
-        <div className="column-buttons col-bttn" >
-          <button
-            type='button'
-            className="column-bttn"
-            onClick={handleCreateTask}
-          >
-            {t('TASKS.ADD')}
-          </button>
-          <button
-            type='button'
-            className="column-bttn"
-            onClick={() => onDelete(column)}
-          >
-            {t('TASKS.DEL')}
-          </button>
-        </div>
-      </div>
+          <div className="column">
+            <h1 className="column-title">{column.title} (order: {column.order})</h1>
 
-    </section>
+            <button
+              type='button'
+              className="column-del"
+              onClick={() => onDelete(column)}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="board-del-icon">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+            </button>
+
+            <div className="column-body" >
+              {tasks && tasks.map(task =>
+                <Task
+                  task={task}
+                  //   boardId = {boardId}
+                  //   colId = {column.id}
+                  onDelete={handleDeleteTask}
+                  key={task.id}
+                />,
+              )}
+            </div>
+            <div className="column-buttons col-bttn" >
+              <button
+                type='button'
+                className="column-bttn"
+                onClick={handleCreateTask}
+              >
+                {t('TASKS.ADD')}
+              </button>
+              <button
+                type='button'
+                className="column-bttn"
+                onClick={() => onDelete(column)}
+              >
+                {t('TASKS.DEL')}
+              </button>
+            </div>
+          </div>
+        </section>
+      )}
+    </Draggable>
   );
 };
--- src/pages/Board/ColumnsContainer/ColumnsContainer.tsx
+++ src/pages/Board/ColumnsContainer/ColumnsContainer.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { useTranslation } from 'react-i18next';
 
 import { Column } from '../Column/Column';
@@ -40,6 +43,10 @@ export const ColumnsContainer = ({ boardId }: ColumnsContainerProps): JSX.Elemen
     }
   };
 
+  const handleDragEnd = async () => {
+    // ToDo
+  };
+
   const handleDelete = async (col: IColumn) => {
     await deleteCol({
       col,
@@ -68,44 +75,57 @@ export const ColumnsContainer = ({ boardId }: ColumnsContainerProps): JSX.Elemen
 
       {((isLoading || delLoading || crLoading) && <Loader/> )}
 
-      <div className="columns-wrapper">
+      <DragDropContext
+        onDragEnd={handleDragEnd} >
+        <Droppable droppableId={boardId}>
+          {provided => (
+            <div className="columns-wrapper"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
 
-        {columns && [...columns]
-          .sort((a, b)=>(a.order - b.order))
-          .map(col =>
-            <Column
-              column = {col}
-              boardId = {boardId}
-              onDelete={handleDelete}
-              key = {col.id}
-            />,
+              {columns && [...columns]
+                .sort((a, b) => (a.order - b.order))
+                .map((col, i) =>
+                  <Column
+                    column={col}
+                    boardId={boardId}
+                    onDelete={handleDelete}
+                    key={col.id}
+                    index={i}
+                  />,
+                )}
+
+              {provided.placeholder}
+
+              <div className="columns-add">
+                <button
+                  type='button'
+                  className='columns-add-bttn'
+                  onClick={handleCreate}
+                >
+                  {t('TASKS.ADD_COL')}
+                </button>
+
+                <button
+                  type='button'
+                  className='columns-add-bttn'
+                  onClick={handleShuffle}
+                >
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                      <path fillRule="evenodd" d="M15.97 2.47a.75.75 0 011.06 0l4.5 4.5a.75.75 0 010 1.06l-4.5 4.5a.75.75 0 11-1.06-1.06l3.22-3.22H7.5a.75.75 0 010-1.5h11.69l-3.22-3.22a.75.75 0 010-1.06zm-7.94 9a.75.75 0 010 1.06l-3.22 3.22H16.5a.75.75 0 010 1.5H4.81l3.22 3.22a.75.75 0 11-1.06 1.06l-4.5-4.5a.75.75 0 010-1.06l4.5-4.5a.75.75 0 011.06 0z" clipRule="evenodd" />
+                    </svg>
+                  </span>
+
+                  <span>{t('TASKS.REVERSE')}</span>
+                </button>
+              </div>
+
+            </div>
           )}
-
-        <div className="columns-add">
-          <button
-            type = 'button'
-            className='columns-add-bttn'
-            onClick={handleCreate}
-          >
-            {t('TASKS.ADD_COL')}
-          </button>
-
-          <button
-            type = 'button'
-            className='columns-add-bttn'
-            onClick={handleShuffle}
-          >
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                <path fillRule="evenodd" d="M15.97 2.47a.75.75 0 011.06 0l4.5 4.5a.75.75 0 010 1.06l-4.5 4.5a.75.75 0 11-1.06-1.06l3.22-3.22H7.5a.75.75 0 010-1.5h11.69l-3.22-3.22a.75.75 0 010-1.06zm-7.94 9a.75.75 0 010 1.06l-3.22 3.22H16.5a.75.75 0 010 1.5H4.81l3.22 3.22a.75.75 0 11-1.06 1.06l-4.5-4.5a.75.75 0 010-1.06l4.5-4.5a.75.75 0 011.06 0z" clipRule="evenodd" />
-              </svg>
-            </span>
-
-            <span>{t('TASKS.REVERSE')}</span>
-          </button>
-        </div>
-
-      </div>
+        </Droppable>
+      </DragDropContext>
     </section>
   );
 };
