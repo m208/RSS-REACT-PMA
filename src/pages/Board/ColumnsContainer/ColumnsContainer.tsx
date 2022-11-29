b52d02556d26b0905b760bfc634496b036391ce1
m208 <avcvet@yandex.ru>
2022-11-29 23:20:19 +0300

    feat: cols dnd

--- src/pages/Board/ColumnsContainer/ColumnsContainer.tsx
+++ src/pages/Board/ColumnsContainer/ColumnsContainer.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext,  Droppable, DropResult } from 'react-beautiful-dnd';
 import { useTranslation } from 'react-i18next';
 
 import { Column } from '../Column/Column';
@@ -43,8 +43,27 @@ export const ColumnsContainer = ({ boardId }: ColumnsContainerProps): JSX.Elemen
     }
   };
 
-  const handleDragEnd = async () => {
-    // ToDo
+  const handleDragEnd = async (result: DropResult) => {
+    console.log(result);
+    const columnsCopy = columns!.map(el=>(({ ...el })));
+
+    const dragged = columnsCopy.find(el=>(el.id === result.draggableId));
+    const destination = columnsCopy.find(el=>(el.id === result.destination?.droppableId));
+    console.log(dragged, destination);
+
+    if (!destination || !dragged) return;
+
+    // const temp = dragged.order;
+    // dragged.order = destination.order;
+    // destination.order = temp;
+
+    [dragged.order, destination.order] = [destination.order, dragged.order];
+
+    await updateColOrder(columnsCopy.map(el=>({
+      _id: el.id,
+      order: el.order,
+    })));
+
   };
 
   const handleDelete = async (col: IColumn) => {
@@ -76,55 +95,59 @@ export const ColumnsContainer = ({ boardId }: ColumnsContainerProps): JSX.Elemen
       {((isLoading || delLoading || crLoading) && <Loader/> )}
 
       <DragDropContext
-        onDragEnd={handleDragEnd} >
-        <Droppable droppableId={boardId}>
-          {provided => (
-            <div className="columns-wrapper"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
+        onDragEnd={
+          (result: DropResult)=>handleDragEnd(result)
+        } >
 
-              {columns && [...columns]
-                .sort((a, b) => (a.order - b.order))
-                .map((col, i) =>
-                  <Column
-                    column={col}
-                    boardId={boardId}
-                    onDelete={handleDelete}
-                    key={col.id}
-                    index={i}
-                  />,
+        <div className='columns-wrapper'>
+
+          {columns && [...columns]
+            .sort((a, b) => (a.order - b.order))
+            .map((col, i) => (
+              <Droppable key={col.id} droppableId={col.id} >
+                {provided => (
+                  <div ref={provided.innerRef}>
+                    <Column
+                      //  ref={provided.innerRef}
+                      column={col}
+                      boardId={boardId}
+                      onDelete={handleDelete}
+                      key= {col.id}
+                      index={i}
+                    />
+                  </div>
+
                 )}
+              </Droppable>
+            ),
+            )}
 
-              {provided.placeholder}
+          <div className="columns-add">
+            <button
+              type='button'
+              className='columns-add-bttn'
+              onClick={handleCreate}
+            >
+              {t('TASKS.ADD_COL')}
+            </button>
 
-              <div className="columns-add">
-                <button
-                  type='button'
-                  className='columns-add-bttn'
-                  onClick={handleCreate}
-                >
-                  {t('TASKS.ADD_COL')}
-                </button>
+            <button
+              type='button'
+              className='columns-add-bttn'
+              onClick={handleShuffle}
+            >
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                  <path fillRule="evenodd" d="M15.97 2.47a.75.75 0 011.06 0l4.5 4.5a.75.75 0 010 1.06l-4.5 4.5a.75.75 0 11-1.06-1.06l3.22-3.22H7.5a.75.75 0 010-1.5h11.69l-3.22-3.22a.75.75 0 010-1.06zm-7.94 9a.75.75 0 010 1.06l-3.22 3.22H16.5a.75.75 0 010 1.5H4.81l3.22 3.22a.75.75 0 11-1.06 1.06l-4.5-4.5a.75.75 0 010-1.06l4.5-4.5a.75.75 0 011.06 0z" clipRule="evenodd" />
+                </svg>
+              </span>
 
-                <button
-                  type='button'
-                  className='columns-add-bttn'
-                  onClick={handleShuffle}
-                >
-                  <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                      <path fillRule="evenodd" d="M15.97 2.47a.75.75 0 011.06 0l4.5 4.5a.75.75 0 010 1.06l-4.5 4.5a.75.75 0 11-1.06-1.06l3.22-3.22H7.5a.75.75 0 010-1.5h11.69l-3.22-3.22a.75.75 0 010-1.06zm-7.94 9a.75.75 0 010 1.06l-3.22 3.22H16.5a.75.75 0 010 1.5H4.81l3.22 3.22a.75.75 0 11-1.06 1.06l-4.5-4.5a.75.75 0 010-1.06l4.5-4.5a.75.75 0 011.06 0z" clipRule="evenodd" />
-                    </svg>
-                  </span>
+              <span>{t('TASKS.REVERSE')}</span>
+            </button>
+          </div>
 
-                  <span>{t('TASKS.REVERSE')}</span>
-                </button>
-              </div>
+        </div>
 
-            </div>
-          )}
-        </Droppable>
       </DragDropContext>
     </section>
   );
