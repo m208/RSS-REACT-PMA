e03e2391beedc1bbd24b2512e86e4c4dc59abc71
TatianaRusak <67562837+TatianaRusak@users.noreply.github.com>
2022-12-05 15:44:15 +0300

    Revert "Alternative backend"

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = 'https://rss-kanban-backend.onrender.com';
+export const API_ENDPOINT = 'https://rss-kanban-backend-production.up.railway.app';
 
 export const tokenExpirationValue = 4 * 60 * 60 * 1000;  // 4 hours in ms
 
